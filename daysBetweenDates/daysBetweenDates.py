3d2087b5a7ebc4ba033f4754f1805cffc2dbb5b2
hooper <hooperzao@163.com>
2017-08-25 23:07:53 +0800

    finished daysBetweenDates

--- daysBetweenDates/daysBetweenDates.py
+++ daysBetweenDates/daysBetweenDates.py
@@ -4,13 +4,39 @@
 # @Author  : Hooper Chao @ SHOU
 # @File    : daysBetweenDates
 # @Software: PyCharm
+def isLeapYear(year):
+    # from Wikipedia
+    # if (year is not divisible by 4) then (it is a common year)
+    # else if (year is not divisible by 100) then (it is a leap year)
+    # else if (year is not divisible by 400) then (it is a common year)
+    # else (it is a leap year)
+    if year % 400 == 0:
+        return True
+    if year % 100 == 0:
+        return False
+    if year % 4 == 0:
+        return True
+    return False
+
+
 def daysInMonth(year, month):
+    if month == 1 or month == 3 or month == 5 or month == 7 \
+            or month == 8 or month == 10 or month == 12:
+        return 31
+    else:
+        if month == 2:
+            if isLeapYear(year):
+                return 29
+            else:
+                return 28
+        else:
+            return 30
     return 30
 
 
 def nextDay(year, month, day):
     """Warning: this version incorrectly assumes all months have 30 days!"""
-    if day < 30:
+    if day < daysInMonth(year, month):
         return year, month, day + 1
     else:
         if month < 12:
@@ -46,3 +72,16 @@ def daysBetweenDates(year1, month1, day1, year2, month2, day2):
 def test():
     # tests with 30-day months!
     assert daysBetweenDates(2013, 1, 1, 2013, 1, 1) == 0
+    assert daysBetweenDates(2013, 1, 1, 2013, 1, 2) == 1
+    assert nextDay(2013, 1, 1) == (2013, 1, 2)
+    assert nextDay(2013, 4, 30) == (2013, 5, 1)
+    assert nextDay(2012, 12, 31) == (2013, 1, 1)
+    assert nextDay(2013, 2, 28) == (2013, 3, 1)
+    assert nextDay(2013, 9, 30) == (2013, 10, 1)
+    assert nextDay(2012, 2, 28) == (2012, 2, 29)
+    assert daysBetweenDates(2012, 1, 1, 2013, 1, 1) == 366
+    assert daysBetweenDates(2013, 1, 1, 2014, 1, 1) == 365
+    assert daysBetweenDates(2013, 1, 24, 2013, 6, 29) == 156
+    print "Test finished"
+
+test()
