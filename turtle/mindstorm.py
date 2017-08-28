1cf2c2e679b520c69d8ce5136d7e729fd001c53b
hooperzao <hooperzao@gmail.com>
2017-08-28 21:37:39 +0800

    added turtle

--- turtle/mindstorm.py
+++ turtle/mindstorm.py
@@ -1,24 +1,32 @@
 import turtle
 
 
-def draw_square():
+def draw_shape(shape):
     window = turtle.Screen()
     window.bgcolor('blue')
-
     scarlet = turtle.Turtle()
     scarlet.shape('turtle')
     scarlet.color('green')
     scarlet.speed(2)
-    scarlet.forward(100)
-    scarlet.right(90)
-    scarlet.forward(100)
-    scarlet.right(90)
-    scarlet.forward(100)
-    scarlet.right(90)
-    scarlet.forward(100)
-    scarlet.right(90)
+
+    if shape == 'rec':
+        num = 0
+        while num < 4:
+            scarlet.forward(100)
+            scarlet.right(90)
+            num += 1
+
+    if shape == 'cir':
+        scarlet.circle(100)
+
+    if shape == 'tri':
+        num = 0
+        while num < 3:
+            scarlet.forward(100)
+            scarlet.right(120)
+            num += 1
 
     window.exitonclick()
 
 
-draw_square()
+draw_shape('tri')
