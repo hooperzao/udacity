eb9701a577f1ae298232704a9efd36e49869418c
hooperzao <hbzhao@shou.edu.cn>
2017-09-01 11:19:45 +0800

    added P2

---
+++ 机器学习（入门）纳米学位/P2/entertainment_center.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 001/9/1/17 10:47
+# @Author  : Hooper Chao @ SHOU
+# @File    : entertainment_center
+# @Software: PyCharm
---
+++ 机器学习（入门）纳米学位/P2/media.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 001/9/1/17 10:47
+# @Author  : Hooper Chao @ SHOU
+# @File    : media
+# @Software: PyCharm
+class Movie():
+    def __init__(self, title, year_of_released, director, type, story_line, poster, trailer):
